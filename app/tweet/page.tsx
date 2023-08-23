821ec37d47bcaf2f612e90d4d5a74b0719916117
sarthakroy107 <sarthakroy107@gmail.com>
2023-08-23 23:25:19 +0530

    First commit with basic apollo/client setup

---
+++ app/tweet/page.tsx
@@ -0,0 +1,38 @@
+import { getClient } from "../lib/client";
+
+import { gql } from "@apollo/client";
+
+// const query = gql`
+//     query {
+//         characters {
+//             results {
+//                 id
+//                 name
+//                 image
+//             }
+//         }
+//     }
+// `
+const query = gql`
+    query Query {
+    fetchUsers {
+        _id
+        bio
+        blue
+        createdAt
+        email
+        likes
+        name
+        password
+        profileImageUrl
+        updatedAt
+    }
+}
+`
+
+export default async function Page() {
+    const {loading, data} = await getClient().query({ query })
+    console.log(data)
+    if(loading) return <main>Loading..</main>
+    return <main className="text-white">{data.fetchUsers[0].email}</main>;
+}
